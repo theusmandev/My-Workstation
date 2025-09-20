8fd975d44526d78ad8aa244b9c908154d2ef85d9
theusmandev <theusmandeveloper@gmail.com>
2025-09-20 21:12:20 +0500

    20-9-2025

--- Database/Library Managment System/app.py
+++ Database/Library Managment System/app.py
@@ -38,7 +38,7 @@ def add_book():
         return redirect(url_for("home"))
     return render_template("add_book.html")
 
-# ✅ E
+
 @app.route("/edit/<int:book_id>", methods=["GET", "POST"])
 def edit_book(book_id):
     conn = get_connection()
